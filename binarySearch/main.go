e674515418c3fd1486ec38225f131005a6a176f9
Said Rehouni <said_rehouni@hotmail.com>
2021-05-20 12:19:54 +0200

    Added binary search recursive algorithm

--- binarySearch/main.go
+++ binarySearch/main.go
@@ -6,6 +6,7 @@ func main() {
 	array := []int{1, 2, 5, 6, 7, 19, 20, 32, 38, 45, 56, 123}
 
 	fmt.Println(binarySearch(array, 19))
+	fmt.Println(binarySearchRecursive(array, 19, 0, len(array)-1))
 }
 
 func binarySearch(array []int, num int) int {
@@ -25,4 +26,24 @@ func binarySearch(array []int, num int) int {
 	}
 
 	return -1
+}
+
+func binarySearchRecursive(array []int, num int, first int, last int) int {
+	mid := (first + last) / 2
+
+	if first > last {
+		return -1
+	}
+
+	if array[mid] == num {
+		return mid
+	}
+
+	if array[mid] > num {
+		last = mid - 1
+	} else {
+		first = mid + 1
+	}
+
+	return binarySearchRecursive(array, num, first, last)
 }
--- binarySearch/main_test.go
+++ binarySearch/main_test.go
@@ -15,11 +15,33 @@ func TestBinary_search_found(t *testing.T) {
 	}
 }
 
+func TestBinary_search_recursive_found(t *testing.T) {
+	array := []int{1, 3, 5, 6, 7, 9, 10, 11, 23, 34, 43, 234, 345, 2323}
+	num := 23
+
+	index := binarySearchRecursive(array, num, 0, len(array)-1)
+
+	if index != 8 {
+		t.Errorf("Expected 8, got %d", index)
+	}
+}
+
 func TestBinary_search_not_found(t *testing.T) {
 	array := []int{1, 3, 5, 6, 7, 9, 10, 11, 23, 34, 43, 234, 345, 2323}
 	num := 24
 
 	index := binarySearch(array, num)
+
+	if index != -1 {
+		t.Errorf("Expected -1, got %d", index)
+	}
+}
+
+func TestBinary_search_recursive_not_found(t *testing.T) {
+	array := []int{1, 3, 5, 6, 7, 9, 10, 11, 23, 34, 43, 234, 345, 2323}
+	num := 24
+
+	index := binarySearchRecursive(array, num, 0, len(array)-1)
 
 	if index != -1 {
 		t.Errorf("Expected -1, got %d", index)
@@ -37,6 +59,17 @@ func TestBinary_search_empty_array(t *testing.T) {
 	}
 }
 
+func TestBinary_search_recursive_empty_array(t *testing.T) {
+	array := []int{}
+	num := 24
+
+	index := binarySearchRecursive(array, num, 0, len(array)-1)
+
+	if index != -1 {
+		t.Errorf("Expected -1, got %d", index)
+	}
+}
+
 func TestBinary_search_one_item_found_array(t *testing.T) {
 	array := []int{2}
 	num := 2
@@ -48,11 +81,33 @@ func TestBinary_search_one_item_found_array(t *testing.T) {
 	}
 }
 
+func TestBinary_search_recursive_one_item_found_array(t *testing.T) {
+	array := []int{2}
+	num := 2
+
+	index := binarySearchRecursive(array, num, 0, len(array)-1)
+
+	if index != 0 {
+		t.Errorf("Expected 0, got %d", index)
+	}
+}
+
 func TestBinary_search_one_item_not_found_array(t *testing.T) {
 	array := []int{2}
 	num := 3
 
 	index := binarySearch(array, num)
+
+	if index != -1 {
+		t.Errorf("Expected -1, got %d", index)
+	}
+}
+
+func TestBinary_search_recursive_one_item_not_found_array(t *testing.T) {
+	array := []int{2}
+	num := 3
+
+	index := binarySearchRecursive(array, num, 0, len(array)-1)
 
 	if index != -1 {
 		t.Errorf("Expected -1, got %d", index)
