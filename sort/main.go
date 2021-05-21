211519561407c54d823e42006a30a3a2391de267
Said Rehouni <said_rehouni@hotmail.com>
2021-05-21 23:44:08 +0200

    Added Merge sort algorithm

--- sort/main.go
+++ sort/main.go
@@ -11,6 +11,9 @@ func main() {
 
 	array1 := []int{13, 3, 2, 6, 6, 8, 23, 11, 20, 34, 25}
 	fmt.Println(QuickSort(array1))
+
+	array2 := []int{13, 3, 2, 6, 6, 8, 23, 11, 20, 34, 25}
+	fmt.Println(MergeSort(array2))
 }
 
 func SelectionSort(array []int) []int {
@@ -60,4 +63,51 @@ func QuickSort(array []int) []int {
 	}
 
 	return append(append(QuickSort(smallerItems), array[pivot]), QuickSort(biggerItems)...)
+}
+
+func MergeSort(array []int) []int {
+	if len(array) < 2 {
+		return array
+	}
+
+	if len(array) == 2 {
+		if array[0] > array[1] {
+			return []int{array[1], array[0]}
+		} else {
+			return array
+		}
+	}
+
+	mid := (len(array) - 1) / 2
+	array1 := MergeSort(array[:mid+1])
+	array2 := MergeSort(array[mid+1:])
+
+	return merge(array1, array2)
+}
+
+func merge(array1 []int, array2 []int) []int {
+	sortedArray := []int{}
+	i := 0
+	j := 0
+
+	for i < len(array1) || j < len(array2) {
+		if i < len(array1) && j < len(array2) {
+			if array1[i] < array2[j] {
+				sortedArray = append(sortedArray, array1[i])
+				i++
+			} else {
+				sortedArray = append(sortedArray, array2[j])
+				j++
+			}
+		} else {
+			if i < len(array1) {
+				sortedArray = append(sortedArray, array1[i])
+				i++
+			} else {
+				sortedArray = append(sortedArray, array2[j])
+				j++
+			}
+		}
+	}
+	return sortedArray
 }
