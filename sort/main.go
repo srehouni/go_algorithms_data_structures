5439f79fbda863b674a8dba5b7df6a0c47627c4f
Said Rehouni <said_rehouni@hotmail.com>
2021-05-21 13:20:03 +0200

    Updated main.go

--- sort/main.go
+++ sort/main.go
@@ -8,71 +8,9 @@ import (
 func main() {
 	array := []int{13, 2, 6, 8, 23, 20, 34, 25}
 	fmt.Println(SelectionSort(array))
-	counter(10)
-	fmt.Println(factorial(10))
-	fmt.Println(sum([]int{1, 2, 3, 4, 5}, 0))
-	fmt.Println(countItems([]int{1, 2, 3, 4, 5}))
-	fmt.Println(maxItem([]int{9, 2, 3, 4, 5}, 1))
-	fmt.Println(maxItem2([]int{9, 2, 3, 4, 5}))
-}
 
-func counter(num int) {
-	fmt.Println(num)
-	if num > 0 {
-		counter(num - 1)
-	}
-}
-
-func factorial(num int) int {
-	if num <= 1 {
-		return 1
-	} else {
-		return num * factorial(num-1)
-	}
-}
-
-func sum(array []int, index int) int {
-	if index == len(array)-1 {
-		return array[index]
-	}
-	return array[index] + sum(array, index+1)
-}
-
-func countItems(array []int) int {
-	if len(array) == 1 {
-		return 1
-	}
-	return 1 + countItems(array[1:])
-}
-
-func maxItem(array []int, max int) int {
-	if max < array[0] {
-		max = array[0]
-	}
-
-	if len(array) == 1 {
-		return max
-	}
-
-	return maxItem(array[1:], max)
-}
-
-func maxItem2(array []int) int {
-	if len(array) == 2 {
-		if array[0] > array[1] {
-			return array[0]
-		} else {
-			return array[1]
-		}
-	}
-
-	max := maxItem2(array[1:])
-
-	if max > array[0] {
-		return max
-	} else {
-		return array[0]
-	}
+	array1 := []int{13, 3, 2, 6, 6, 8, 23, 11, 20, 34, 25}
+	fmt.Println(QuickSort(array1))
 }
 
 func SelectionSort(array []int) []int {
