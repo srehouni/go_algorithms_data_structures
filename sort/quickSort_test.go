0d7dbb87c3ac24856ed7861183bfe68940420267
Said Rehouni <said_rehouni@hotmail.com>
2021-05-21 13:19:47 +0200

    Added test case

--- sort/quickSort_test.go
+++ sort/quickSort_test.go
@@ -54,3 +54,13 @@ func TestQuickSort(t *testing.T) {
 		t.Errorf("Expected [2 3, 6, 6, 8, 11, 13, 20, 23, 25, 34], got %d", sortedArray)
 	}
 }
+
+func TestQuickSort_test1(t *testing.T) {
+	array := []int{13, 2, 6, 8, 23, 20, 34, 25}
+
+	sortedArray := QuickSort(array)
+
+	if !reflect.DeepEqual(sortedArray, []int{2, 6, 8, 13, 20, 23, 25, 34}) {
+		t.Errorf("Expected [2, 6, 8, 13, 20, 23, 25, 34], got %d", sortedArray)
+	}
+}
